7da4bd4beebcd2ba666231cae3177867aa5a232f
Илья Халяпин <962426@mail.ru>
2018-01-08 10:08:49 +0300

    prepare project

---
+++ ToDo/Classes/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  ToDo
+//
+//  Created by Илья Халяпин on 07.01.2018.
+//  Copyright © 2018 Илья Халяпин. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - Публичные свойства
+    
+    var window: UIWindow?
+    
+    
+    // MARK: - UIApplicationDelegate
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
